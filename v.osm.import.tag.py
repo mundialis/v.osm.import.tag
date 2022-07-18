184af3abc68b50d711e3156775b61aed7688c645
Johannes Halbauer <halbauer@terrestris.de>
2022-07-18 13:57:35 +0000

    v.extract.osm: functional changes

---
+++ v.osm.import.tag.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+#
+############################################################################
+#
+# MODULE:      v.osm.import.tag
+# AUTHOR(S):   Johannes Halbauer
+#
+# PURPOSE:     Extracts all data selected by a tag from OSM wihtin a defined area
+# COPYRIGHT:   (C) 2022 by mundialis GmbH & Co. KG and the GRASS Development Team and the Overpass API Development Team
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#############################################################################
+
+# %Module
+# % description: Extracts all data selected by an OSM tag (through the Overpass API as inpuit source) within a specified area (geojson or vector in GRASS as input) and outputs a geojson and a vector file in GRASS.
+# % keyword: vector
+# % keyword: roads
+# % keyword: OSM
+# %end
+
+# %option G_OPT_V_INPUT
+# % key: map
+# % required: no
+# % description: Existing vector AOI map in GRASS as input
+# %end
+
+# %option G_OPT_F_INPUT
+# % key: geojson
+# % required: no
+# % description: Name of AOI geojson file as input
+# %end
+
+# %option
+# % key: osm_tag
+# % required: yes
+# % description: OSM tag for querying a specific part of OSM data (form: 'way["highway"]')
+# %end
+
+# %option G_OPT_V_OUTPUT
+# % key: output
+# % required: yes
+# % multiple: no
+# % description: Output name of processing result vector map
+# %end
+
+# %rules
+# % exclusive: map, geojson
+# %end
+
+
+import json
+import os
+import atexit
+import shutil
+import grass.script as grass
+
+options, flags = grass.parser()
+
+##### define variables
+
+temp_dir = grass.tempdir()
+
+# create string for coordinates
+coord_strings = ""
+
+# define output file name
+output = options["output"]
+
+# osm element and tag to query
+osm_tag = options["osm_tag"]
+
+
+
+
+#### define functions
+
+
+def check_geojson(input_json):
+    """Check if there is a feature contained in geojson"""
+    if len(input_json["features"]) == 0:
+        grass.message(_("Stopped, because of no feature contained in geojson."))
+        quit()
+
+    elif len(input_json["features"]) > 1:
+        grass.message(_("There are more than one feature in the geojson."))
+        grass.message(_("Only the first feature will be used!"))
+        grass.message(_("Starting to process..."))
+
+    else:
+        grass.message(_("Starting to process..."))
+
+
+def coords_format(coordinates):
+    """change structure of coordinates to "x y x y ..." needed for the overpass query"""
+    global coord_strings
+
+    coord_strings = []
+
+    for coordinate in coordinates:
+        x = coordinate[0]
+        y = coordinate[1]
+        poly = f"{y} {x}"
+        coord_strings.append(poly)
+
+    coord_strings = " ".join(coord_strings)
+
+def cleanup():
+    if os.path.isdir(temp_dir):
+        shutil.rmtree(temp_dir)
+
+
+#### processing
+
+
+def main():
+
+    global temp_dir
+
+    if not grass.find_program("v.out.geojson", "--help"):
+        grass.fatal(
+            _("The 'v.out.geojson' addon module was not found, " "install it first:")
+            + "\n"
+            + "g.extension v.out.geojson url=path/to/addon"
+        )
+
+    tmp_geojson = os.path.join(temp_dir, "tmp.geojson")
+
+    # lazy import nonstandard module
+    try:
+        import overpass
+    except ImportError as e:
+        grass.fatal(_("Module requires overpass library: {}").format(e))
+
+    # instance api with timeout after 700 seconds
+    api = overpass.API(timeout=700)
+
+    area_of_interest = options["geojson"]
+    map = options["map"]
+    if map:
+        grass.run_command(
+            "v.out.geojson", input=map, output=tmp_geojson
+        )
+
+    # load AOI (geojson) as dict
+    input_geojson = None
+    if map:
+        with open(tmp_geojson, mode="r") as f:
+            input_geojson = json.load(f)
+    else:
+        with open(f"{area_of_interest}", mode="r") as f:
+            input_geojson = json.load(f)
+
+    check_geojson(input_geojson)
+
+    coords_format(input_geojson["features"][0]["geometry"]["coordinates"][0])
+
+    # rename variable
+    my_area = coord_strings
+
+    # define query to get result
+    # query = f'way["highway"](poly:"{my_area}");'
+
+    query = f'{osm_tag}(poly:"{my_area}");'
+
+    # send request to overpass api
+    result = api.get(query, verbosity="geom")
+
+    attributes = list()
+    for i, feature in enumerate(result["features"]):
+        if not isinstance(feature, dict):
+            continue
+        attribute_names_lower = [attr.lower() if isinstance(attr, str) else str(attr) for attr in feature["properties"]]
+        attribute_names = [attr for attr in feature["properties"] if isinstance(attr, str)]
+
+        for attribute in attribute_names:
+            if attribute.lower() in attributes and attribute != attribute.lower():
+                new_name = f"{attribute.lower()}"
+                if attribute.lower() in attribute_names_lower:
+                    while new_name in attribute_names_lower:
+                        new_name += "2"
+                result["features"][i]["properties"][new_name] = feature["properties"][attribute]
+                del result["features"][i]["properties"][attribute]
+                attribute_names_lower.append(new_name)
+
+        attributes.extend(attribute_names_lower)
+
+    output_geojson = os.path.join(temp_dir, f"{output}.geojson")
+
+
+    # write request result to geojson
+    with open(output_geojson, mode="w") as f:
+        json.dump(result, f)
+
+
+    grass.run_command("v.import", input=output_geojson, output=output)
+
+    grass.message(_("Done"))
+
+
+if __name__ == "__main__":
+    options, flags = grass.parser()
+    atexit.register(cleanup)
+    main()
